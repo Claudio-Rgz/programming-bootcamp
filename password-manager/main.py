ea55c11e4b1cd31aa06207c4ce3a722b0e523f84
Claudio Rodriguez <rgz.claudio@gmail.com>
2024-10-17 08:57:53 -0600

    Update project with search functionality

--- password-manager/main.py
+++ password-manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 from pyperclip import copy
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -82,24 +83,71 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    if len(website_entry.get()) == 0 or len(password_display.get()) == 0:
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_display.get()
+    new_data = {
+        website.lower(): {
+            "email": email.lower(),
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or len(password) == 0:
         blank_entry = messagebox.showerror(
             "Missing Information", "Please don't leave any fields empty!"
         )
     else:
-        confirmation = messagebox.askyesno(
-            title=f"{website_entry.get()}",
-            message=f"These are the the details entered: \nEmail: {email_entry.get()}"
-            f"\nPassword: {password_display.get()} \nIs it ok to save?",
-        )
-        if confirmation == True:
-            with open("database.txt", "a") as f:
-                f.write(
-                    f"{website_entry.get()} | {email_entry.get()} | {password_display.get()}\n"
-                )
-
+        try:
+            with open("database.json", "r") as f:
+                # Read the information from our database
+                data = json.load(f)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("database.json", "w") as f:
+                json.dump(new_data, f, indent=4)
+        else:
+            with open("database.json", "w") as f:
+                json.dump(data, f, indent=4)
+        finally:
             website_entry.delete(0, "end")
             password_display.delete(0, "end")
+
+
+# ------------------------- SEARCH PASSWORD ---------------------------- #
+def search():
+    website = website_entry.get()
+    website = website.lower()
+
+    if len(website) == 0:
+        messagebox.showerror(
+            "Missing Information", "Please don't leave the website field empty!"
+        )
+    else:
+        try:
+            with open("database.json", "r") as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            messagebox.showerror(
+                title="Error",
+                message="No DataFile found.",
+            )
+        else:
+            try:
+                email = data[website]["email"]
+                password = data[website]["password"]
+
+                copy(password)
+
+                messagebox.showinfo(
+                    title="Information",
+                    message=f"Website: {website.title()}\nEmail: {email}\nPassword: {password}",
+                )
+            except KeyError:
+                messagebox.showerror(
+                    title="Error",
+                    message="There's no saved password for that webpage.",
+                )
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -114,17 +162,30 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # Website Label
-website_txt = Label(text="Website:", font=("Arial", 8, "normal"), background="white")
+website_txt = Label(
+    text="Website:", pady=5, font=("Arial", 8, "normal"), background="white"
+)
 website_txt.grid(column=0, row=1)
 
 # Website entry
 website_entry = Entry()
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_entry.grid(column=1, row=1, sticky="EW")
+
+# Search button
+search_button = Button(
+    text="Search",
+    width=16,
+    # pady=5,
+    font=("Arial", 8, "normal"),
+    background="white",
+    command=search,
+)
+search_button.grid(column=2, row=1)
 
 # Email Label
 email_txt = Label(
-    text="Email/Username:", font=("Arial", 8, "normal"), background="white"
+    text="Email/Username:", pady=5, font=("Arial", 8, "normal"), background="white"
 )
 email_txt.grid(column=0, row=2)
 
@@ -134,7 +195,9 @@ email_entry.insert(0, "rgz.claudio@gmail.com")
 email_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
 
 # Password text
-password_txt = Label(text="Password:", font=("Arial", 8, "normal"), background="white")
+password_txt = Label(
+    text="Password:", pady=5, font=("Arial", 8, "normal"), background="white"
+)
 password_txt.grid(column=0, row=3)
 
 # Password display
@@ -144,6 +207,8 @@ password_display.grid(column=1, row=3, sticky="EW")
 # Password generator button
 password_button = Button(
     text="Generate Password",
+    width=16,
+    # pady=5,
     font=("Arial", 8, "normal"),
     background="white",
     command=generate_password,
@@ -152,7 +217,12 @@ password_button.grid(column=2, row=3)
 
 # Add button
 add_button = Button(
-    text="Add", font=("Arial", 8, "normal"), background="white", width=50, command=save
+    text="Add",
+    # pady=5,
+    font=("Arial", 8, "normal"),
+    background="white",
+    width=50,
+    command=save,
 )
 add_button.grid(column=1, row=4, columnspan=2)
 
